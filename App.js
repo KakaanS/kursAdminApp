137b9ac22ab03866ecc9130b1e6471a68b3f255c
Oscar Wendt <oscar.wendt91@gmail.com>
2023-05-30 13:42:07 +0200

    updated to drawer menu

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { View, Text } from "react-native";
 
 // Screens
@@ -12,17 +14,17 @@ import AddCourse from "./screens/AddCourse";
 // Context
 import { CourseProvider } from "./context/CourseContext";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <CourseProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Courses" component={Courses} />
-          <Tab.Screen name="Add Course" component={AddCourse} />
-        </Tab.Navigator>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Courses" component={Courses} />
+          <Drawer.Screen name="Add Course" component={AddCourse} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </CourseProvider>
   );
